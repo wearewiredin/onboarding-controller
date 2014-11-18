b57dd27601e9e0e9d3a19e05ef2e06c7a3c47c5b
Joshua Howland <development@jkhowland.com>
2014-11-17 23:18:29 -0700

    Initial Commit

---
+++ onboarding-controller/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  onboarding-controller
+//
+//  Created by Joshua Howland on 11/17/14.
+//  Copyright (c) 2014 Wired In LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ onboarding-controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  onboarding-controller
+//
+//  Created by Joshua Howland on 11/17/14.
+//  Copyright (c) 2014 Wired In LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
